016565d88f0f66377e67197f249b819727d8f756
Shreya Dasar <shreyadasar26@gmail.com>
2025-12-13 04:32:58 +0530

    Assignment Question 2

---
+++ Assignment_Day_1/Q2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void print_bin(void *vp, int size);
+void main()
+{
+    
+unsigned char ch=10;
+print_bin(&ch,sizeof(ch));
+
+}
+void print_bin(void *vp,int size)
+{
+   
+   int i;
+
+    for(i = size - 1; i >= 0; i--)
+    {
+        unsigned char mask=0x80;
+    while(mask)
+    {
+             if( *((char *)vp ) & mask )
+        {
+            printf("1");
+        }
+        else
+        {
+            printf("0");
+        }
+         mask = mask >> 1; 
+    }
+    }
+}
